d84cb43477ba6587949cfd36ff126a4301f7eeef
Ryan Kabia <rkabia12@gmail.com>
2019-01-13 18:04:47 +0300

    [chore #163158973]Add User endpoints
    Done:
        *Added signin and signup endpoints that allow user to signup and
         login respectievly
        *Added User data structure to hold user information. Also added
         classes in models file but not implemented yet

--- app/api/v1/views/meetup_view.py
+++ app/api/v1/views/meetup_view.py
@@ -1,36 +1,36 @@
 from flask import Flask, jsonify, request, Blueprint
-from app.api.v1.models.models import Meetup,Question,Rsvp
+from app.api.v1.models.models import Meetups,Questions,Rsvps,Comments
 from app.api.v1.utils.validate import Validate
-from random import randint
 
 #app=Flask(__name__)
-mod1 = Blueprint('api', __name__)
+mod2 = Blueprint('api2', __name__)
+
 vldr = Validate()
 
-@mod1.route('/meetups/upcoming/')
+@mod2.route('/meetups/upcoming/')
 def all_meetups():
-    return jsonify({"status": 200, "data": Meetup}), 200
+    return jsonify({"status": 200, "data": Meetups}), 200
 
 
-@mod1.route('/meetups/<meetup_id>')
+@mod2.route('/meetups/<meetup_id>')
 def specific_meetups(meetup_id):
-    for entry in Meetup:
+    for entry in Meetups:
         if entry["id"] == int(meetup_id):
             return jsonify({"status": 200, "data": entry}), 200
 
     return jsonify ({"status":404,"data":"entry cannot be found"}),404
 
 
-@mod1.route('/meetups',methods = ["POST"])
+@mod2.route('/meetups',methods = ["POST"])
 def create_meetup():
     data = request.get_json()
 
-    last_id=Meetup[-1]["id"]
+    last_id=Meetups[-1]["id"]
     inc_id= last_id+1
 
     id = inc_id 
     if vldr.check_empty(data["createdOn"], data["location"], data["happeningOn"], data["topic"], data["Tags"]) == False:
-        return jsonify({"Message":"empty field"})
+        return jsonify({"status":400,"error":"One of more fields are empty"}),400
     if vldr.check_date(data["createdOn"], data["happeningOn"]) == False:
         return jsonify({"Message": "Bad date format"})
     if vldr.check_valid_date(data["createdOn"], data["happeningOn"]) == False:
@@ -51,20 +51,20 @@ def create_meetup():
         "Tags": Tags,
     }
 
-    Meetup.append(new_meetup)
+    Meetups.append(new_meetup)
     
     return jsonify ({"status":201,"data":[new_meetup]}),201
 
 
-@mod1.route('/questions', methods=["POST"])
+@mod2.route('/questions', methods=["POST"])
 def create_question():
     data = request.get_json()
 
-    last_id = Question[-1]["id"]
+    last_id = Questions[-1]["id"]
     inc_id = last_id+1
 
     if vldr.check_empty(data["createdOn"], data["createdBy"], data["meetup"], data["title"], data["body"]) == False:
-        return jsonify({"Message": "empty field"})
+        return jsonify({"status": 400, "error": "One of more fields are empty"}), 400
     if vldr.check_date(data["createdOn"]) == False:
         return jsonify({"Message": "Bad date format"})
     if vldr.check_valid_date(data["createdOn"]) == False:
@@ -88,21 +88,50 @@ def create_question():
         "votes": votes,
     }
 
-    Question.append(new_question)
+    Questions.append(new_question)
 
     return jsonify({"status": 201, "data": [new_question]}), 201
 
+@mod2.route('/questions/<question_id>/comment',methods=["POST"])
+def create_comment(question_id):
 
-@mod1.route('/meetups/<meetup_id>/rsvp', methods=["POST"])
+    data = request.get_json()
+
+    last_id = Comments[-1]["id"]
+    cinc_id = last_id+1
+
+    for entry in Questions:
+        if entry["id"] == int(question_id):
+            qstn = entry["title"]
+
+    if vldr.check_empty(data["user"], data["responce"]) == False:
+        return jsonify({"status": 400, "error": "One of more fields are empty"}), 400
+    id = cinc_id
+    question = qstn
+    user = data["user"]
+    responce = data["responce"]
+
+    new_comment = {
+        "id": id,
+        "question": question,
+        "user": user,
+        "responce": responce
+    }
+
+    Comments.append(new_comment)
+
+    return jsonify({"status": 201, "data": [new_comment]}), 201
+
+@mod2.route('/meetups/<meetup_id>/rsvp', methods=["POST"])
 def rsvp(meetup_id):
 
     data = request.get_json()
 
-    last_id = Rsvp[-1]["id"]
+    last_id = Rsvps[-1]["id"]
     inc_id = last_id+1
 
     if vldr.check_empty(data["user"], data["responce"]) == False:
-        return jsonify({"Message": "empty field"})
+        return jsonify({"status": 400, "error": "One of more fields are empty"}), 400
     if vldr.check_responce(data["responce"]) == False:
         return jsonify({"Message": "Limit Answers to:Yes, No or Maybe"})
 
@@ -118,9 +147,9 @@ def rsvp(meetup_id):
         "responce":responce
     }
 
-    Rsvp.append(new_rsvp)
+    Rsvps.append(new_rsvp)
 
-    for entry in Meetup:
+    for entry in Meetups:
         if entry["id"] == int(meetup_id):
             topic= entry["topic"]
             break
@@ -130,11 +159,11 @@ def rsvp(meetup_id):
         "topic": topic,
         "responce":responce
     }
-    return jsonify({"status": 201, "data": [rtrn_obj]}), 201
+    return jsonify({"status": 200, "data": [rtrn_obj]}), 200
 
-@mod1.route('/questions/<question_id>/upvote', methods=["PATCH"])
+@mod2.route('/questions/<question_id>/upvote', methods=["PATCH"])
 def upvote(question_id):
-    for entry in Question:
+    for entry in Questions:
         if entry["id"] == int(question_id):
             meetup = entry["meetup"]
             title = entry["title"]
@@ -149,9 +178,9 @@ def upvote(question_id):
                 }
     return jsonify({"status": 201, "data": [rtrn_obj]}), 201
 
-@mod1.route('/questions/<question_id>/downvote', methods=["PATCH"])
+@mod2.route('/questions/<question_id>/downvote', methods=["PATCH"])
 def downvote(question_id):
-    for entry in Question:
+    for entry in Questions:
         if entry["id"] == int(question_id):
             meetup = entry["meetup"]
             title = entry["title"]
